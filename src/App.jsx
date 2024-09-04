a783dcc521fff1c836093cd2dd3c6d33fee778be
AlanPratama <pralan76@gmail.com>
2024-09-04 18:01:38 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { PageLayout } from './layouts/PageLayout'
+import { HomePage } from './pages/HomePage'
+import { LoginPage } from './pages/LoginPage'
+import { RegisterPage } from './pages/RegisterPage'
+
+export const App = () => {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <PageLayout><HomePage/></PageLayout>,
+    }, 
+    {
+        path: "/auth",
+        children: [
+          {
+            path: "login",
+            element: <LoginPage/>
+          },
+          {
+            path: "register",
+            element: <RegisterPage/>
+          }
+        ]
+    },
+  ])
+
+  return (
+    <RouterProvider router={router} />    
+  )
+}
---
+++ src/layouts/PageLayout.jsx
@@ -0,0 +1,8 @@
+export const PageLayout = ({ children }) => {
+  return (
+    <>
+    <header>HEADERR</header>
+    {children}
+    </>
+  )
+}
