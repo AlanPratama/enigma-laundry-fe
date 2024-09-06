686e0de9e38651dda5f851e004dc0fb468b7d9c2
fauzangan <fauzan12356@gmail.com>
2024-09-06 09:12:19 +0700

    fix date

--- src/pages/dashboard/transaction/TransactionPage.jsx
+++ src/pages/dashboard/transaction/TransactionPage.jsx
@@ -13,6 +13,7 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import TransactionApi from "../../../apis/TransactionsApi";
 import ModalComponent from "./components/ModalComponent";
+import { AddCircleOutline, EyeOutline } from "react-ionicons";
 
 function BillPage() {
   const data = useSelector((state) => state.transactions);
@@ -30,15 +31,15 @@ function BillPage() {
   }, []);
 
   const handleCreateTransactions = async (transaction) => {
-    try{
+    try {
       console.log("LSKALKSLA: ", transaction);
-      
-      await TransactionApi.createTransactions(transaction)
-      getTransactions()
+
+      await TransactionApi.createTransactions(transaction);
+      getTransactions();
     } catch (error) {
       console.log("Error creating customers", error.message);
     }
-  }
+  };
 
   return (
     <div className="flex justify-center items-start pt-12 h-screen">
@@ -47,32 +48,28 @@ function BillPage() {
           <h1 className="font-bold text-start text-xl p-2">
             Transaction Table
           </h1>
-          <Button color="primary" onPress={() => {
-            setModalTitle("Tambah Transaksi")
-            setSelectedTransaction(null)
-            setIsCreate(true)
-            onOpen();
-          }}>
-            <ion-icon name="home"></ion-icon> Tambah Transaksi
+          <Button
+            color="primary"
+            onPress={() => {
+              setModalTitle("Tambah Transaksi");
+              setSelectedTransaction(null);
+              setIsCreate(true);
+              onOpen();
+            }}
+          >
+            <AddCircleOutline name="add-circle-outline" color="white" /> Tambah
+            Transaksi
           </Button>
         </CardHeader>
         <CardBody>
           <Table isStriped aria-label="Transaction Table">
             <TableHeader>
-              <TableColumn className="bg-blue-200 text-black">
-                ID Transaksi
-              </TableColumn>
-              <TableColumn className="bg-blue-200 text-black">
-                Customer
-              </TableColumn>
-              <TableColumn className="bg-blue-200 text-black">User</TableColumn>
-              <TableColumn className="bg-blue-200 text-black">
-                Bill Details
-              </TableColumn>
-              <TableColumn className="bg-blue-200 text-black">
-                Transaction Date
-              </TableColumn>
-              <TableColumn className="bg-blue-200 text-black">Aksi</TableColumn>
+              <TableColumn>ID Transaksi</TableColumn>
+              <TableColumn>Customer</TableColumn>
+              <TableColumn>User</TableColumn>
+              <TableColumn>No Hp Customer</TableColumn>
+              <TableColumn>Tanggal Transaksi</TableColumn>
+              <TableColumn>Aksi</TableColumn>
             </TableHeader>
             <TableBody emptyContent={"Tidak ada transaksi."}>
               {data &&
@@ -81,20 +78,23 @@ function BillPage() {
                     <TableCell>{transaction.id}</TableCell>
                     <TableCell>{transaction.customer.name}</TableCell>
                     <TableCell>{transaction.user.name}</TableCell>
-                    <TableCell>{transaction.user.email}</TableCell>
-                    <TableCell>{transaction.billDate}</TableCell>
+                    <TableCell>{transaction.customer.phoneNumber}</TableCell>
+                    <TableCell>
+                      {new Date(transaction.billDate).toLocaleDateString()}{" "}
+                      {new Date(transaction.billDate).toLocaleTimeString("en-GB")}
+                    </TableCell>
                     <TableCell>
                       <Button
                         className="mx-1"
                         color="primary"
                         onPress={() => {
-                          setSelectedTransaction(transaction)
-                          setModalTitle("Detail Transaksi")
-                          setIsCreate(false)
-                          onOpen()
+                          setSelectedTransaction(transaction);
+                          setModalTitle("Detail Transaksi");
+                          setIsCreate(false);
+                          onOpen();
                         }}
                       >
-                        Detail
+                        <EyeOutline color="white" /> Detail
                       </Button>
                     </TableCell>
                   </TableRow>
@@ -105,12 +105,12 @@ function BillPage() {
       </Card>
       {/* Modal */}
       <ModalComponent
-      isOpen={isOpen}
-      onOpenChange={onOpenChange}
-      title={modalTitle}
-      isCreate={isCreate}
-      transaction={selectedTransaction}
-      handleCreateTransaction={handleCreateTransactions}
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        title={modalTitle}
+        isCreate={isCreate}
+        transaction={selectedTransaction}
+        handleCreateTransaction={handleCreateTransactions}
       />
     </div>
   );
